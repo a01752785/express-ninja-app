2a8619dc61c0696c223f287235aebee9215660f9
a01752785 <a01752785@itesm.mx>
2022-04-19 14:33:09 -0500

    Implement POST error handling

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 // Set up express app
 const app = express();
 
+// Connect to mongodb
+mongoose.connect("mongodb://localhost/ninjago");
+mongoose.Promise = global.Promise;
+
+// JSON parser middleware
 app.use(bodyParser.json());
 
 // Initalize routes
 app.use('/api', require("./routes/api"));
+
+// Error handling middleware
+app.use(function(err, req, res, next){
+    res.status(422).send({
+        error:err.message
+    })
+})
 
 // Listen for requests
 app.listen(process.env.port || 4000, function() {
