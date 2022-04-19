2f303322139b4f1ac5037a26a0f1e4cea70751ed
a01752785 <a01752785@itesm.mx>
2022-04-19 13:53:04 -0500

    Create ninja schema

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 // Set up express app
 const app = express();
 
+app.use(bodyParser.json());
+
+// Initalize routes
 app.use('/api', require("./routes/api"));
 
 // Listen for requests
